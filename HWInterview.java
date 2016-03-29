3ea7551190b937a3ca40cf0bf8b3e09b62409aa8
linlingyue <linlingyue@jd.com>
2016-03-29 11:27:07 +0800

    修改bug

--- HWInterview.java
+++ HWInterview.java
@@ -10,38 +10,58 @@ import static java.util.Collections.*;
  */
 public class HWInterview {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        String input ="C7 D9 B4 B2 C5 C6 A9 C9 A4 A5 A6 B1 C1";
+        String input = "A7 B3 A5 A2 A4 A3 A7 A3 A9";
         String[] split = input.split(" ");
         List<String> strings = Arrays.asList(split);
         sort(strings);
 
-        Map<String,Integer> oMap = new HashMap<String, Integer>();
+        Map<String, Integer> oMap = new HashMap<String, Integer>();
+        Map<String, Integer> aMap = new HashMap<String, Integer>();
 
-        for(int i = 0 , length = strings.size() ; i<length ; i++){
-            String pk = strings.get(i);
+        List<String> tmpStrs = new ArrayList<String>();
+        for(String str : strings){
+            if(!tmpStrs.contains(str))
+                tmpStrs.add(str);
+        }
+
+        for (int i = 0; i < tmpStrs.size(); i++) {
+            String pk = tmpStrs.get(i);
 
             char[] strchar = pk.toCharArray();
 
-            if(strchar[0] != 'A'){
-                oMap.put(pk,Integer.valueOf(strchar[1]+""));
+            if (strchar[0] != 'A') {
+                oMap.put(pk, Integer.valueOf(strchar[1] + ""));
+            } else {
+                if(aMap.containsKey(pk)){
+                    tmpStrs.remove(pk);
+                }
+                else{
+                    aMap.put(pk, Integer.valueOf(strchar[1] + ""));
+                }
             }
         }
 
-        List<Map.Entry<String,Integer>> l = new ArrayList<Map.Entry<String, Integer>>(oMap.entrySet());
+        List<Map.Entry<String, Integer>> l = null;
+        if (aMap.size() == tmpStrs.size()) {
+            l = new ArrayList<Map.Entry<String, Integer>>(aMap.entrySet());
+
+        } else {
+            l = new ArrayList<Map.Entry<String, Integer>>(oMap.entrySet());
+        }
+
         sort(l, new Comparator<Map.Entry<String, Integer>>() {
             public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
                 return o1.getKey().compareTo(o2.getKey());
             }
         });
 
-        Collections.sort(l, new Comparator<Map.Entry<String, Integer>>() {
+        sort(l, new Comparator<Map.Entry<String, Integer>>() {
             public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
                 return o1.getValue().compareTo(o2.getValue());
             }
         });
-
-        System.out.println(l.get(l.size() -1).getKey());
+        System.out.println(l.get(l.size() - 1).getKey());
     }
 }
